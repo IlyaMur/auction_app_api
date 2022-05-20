000e7eb474b283304cd433e71aca1481a2d7bbad
Ilya Mur <ilyamur@hotmail.com>
2022-05-20 14:20:47 +0300

    finished auth functionality

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
@@ -9,4 +10,16 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 class ResetPasswordController extends Controller
 {
     use ResetsPasswords;
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        return response()->json([
+            'status' => trans($response)
+        ], 200);
+    }
+
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response()->json(['email' => trans($response)]);
+    }
 }
