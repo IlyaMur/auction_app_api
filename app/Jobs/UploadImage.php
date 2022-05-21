6eba4f5dcdc33c94e1a70cfa3547ffc424b3655d
Ilya Mur <ilyamur@hotmail.com>
2022-05-21 14:25:06 +0300

    add job to resize the user image

--- app/Jobs/UploadImage.php
+++ app/Jobs/UploadImage.php
@@ -4,7 +4,6 @@ namespace App\Jobs;
 
 use App\Models\Models\Design;
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
 use Illuminate\Queue\SerializesModels;
@@ -12,13 +11,15 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class UploadImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $design;
+    protected $disk;
+    protected $filename;
+    protected $originalFile;
 
     /**
      * Create a new job instance.
@@ -28,17 +29,36 @@ class UploadImage implements ShouldQueue
     public function __construct(Design $design)
     {
         $this->design = $design;
+        $this->disk = $design->disk;
+        $this->filename = $design->image;
+
+        $this->originalFile = storage_path()
+            . '/uploads/original/' . $design->image;
     }
 
-    protected function resizeAndSave($originalFile, $pathToSave, $resolutin)
+    protected function resizeAndSave($width, $height, $size)
     {
-        Image::make($originalFile)
-            ->fit(800, 600, function ($constraint) {
+        $pathToSave = storage_path(
+            "uploads/$size/" . $this->filename
+        );
+
+        Image::make($this->originalFile)
+            ->fit($width, $height, function ($constraint) {
                 $constraint->aspectRatio();
             })
-            ->save($large = storage_path(
-                $pathToSave . $this->design->image
-            ));
+            ->save($pathToSave);
+
+        $this->storeFile($size, $pathToSave);
+    }
+
+    protected function storeFile($size, $file)
+    {
+        if (Storage::disk($this->disk)->put(
+            "uploads/designs/$size/" . $this->filename,
+            fopen($file, 'r+')
+        )) {
+            File::delete($file);
+        }
     }
 
     /**
@@ -48,45 +68,13 @@ class UploadImage implements ShouldQueue
      */
     public function handle()
     {
-        $disk = $this->design->disk;
-        $filename = $this->design->image;
-
-        $originalFile = storage_path()
-            . '/uploads/original/'
-            . $this->design->image;
         try {
             // create the large image and save to tmp disk
-            Image::make($originalFile)
-                ->fit(800, 600, function ($constraint) {
-                    $constraint->aspectRatio();
-                })
-                ->save($large = storage_path(
-                    'uploads/large' . $filename
-                ));
-
-            // create the thumbnail image and save to tmp disk
-            Image::make($originalFile)
-                ->fit(250, 200, function ($constraint) {
-                    $constraint->aspectRatio();
-                })
-                ->save($thumbnail = storage_path(
-                    'uploads/thumbnail' . $filename
-                ));
-
-            // store images to permanent location
-            if (Storage::disk($disk)->put('uploads/designs/original/' . $filename, fopen($originalFile, 'r+'))) {
-                File::delete($originalFile);
-            }
-
-            // large images
-            if (Storage::disk($disk)->put('uploads/designs/large/' . $filename, fopen($large, 'r+'))) {
-                File::delete($large);
-            }
-
-            // thumbnail images
-            if (Storage::disk($disk)->put('uploads/designs/thumbnail/' . $filename, fopen($thumbnail, 'r+'))) {
-                File::delete($thumbnail);
-            }
+            $this->resizeAndSave(800, 600, 'large');
+            // create the thumbnail image
+            $this->resizeAndSave(250, 200, 'thumbnail');
+            // save the original image
+            $this->storeFile('original', $this->originalFile);
 
             // Update the db record
             $this->design->update(['upload_successful' => true]);
