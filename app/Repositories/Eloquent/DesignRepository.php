f6d03966941a53cc1a4fe48f357600d6144e8b08
Ilya Mur <ilyamur@hotmail.com>
2022-05-23 15:17:31 +0300

    reformat code

--- app/Repositories/Eloquent/DesignRepository.php
+++ app/Repositories/Eloquent/DesignRepository.php
@@ -2,14 +2,13 @@
 
 namespace App\Repositories\Eloquent;
 
-use App\Models\Design;
 use App\Repositories\Eloquent\BaseRepository;
 use App\Repositories\Contracts\DesignInterface;
 
 class DesignRepository extends BaseRepository implements DesignInterface
 {
-    // public function model()
-    // {
-    //     return Design::class;
-    // }
+    public function model()
+    {
+        return Design::class;
+    }
 }
