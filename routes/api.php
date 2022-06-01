95510d44183175af39ee463ef816214b717b2e2f
Ilya Mur <ilyamur@hotmail.com>
2022-06-01 18:19:29 +0300

    refactor routes file

--- routes/api.php
+++ routes/api.php
@@ -19,72 +19,87 @@ use App\Http\Controllers\Chats\ChatsController;
 /**
  * Public routes
  */
+
 // Get current auth user
 Route::get('me', [MeController::class, 'getMe']);
 
-Route::get('designs', [DesignController::class, 'index']);
-Route::get('designs/{id}', [DesignController::class, 'findDesign']);
-Route::get('designs/slug/{slug}', [DesignController::class, 'findBySlug']);
+Route::controller(DesignController::class)->group(function () {
+    Route::get('designs', 'index');
+    Route::get('designs/{id}', 'findDesign');
+    Route::get('designs/slug/{slug}', 'findBySlug');
+    Route::get('users/{id}/designs', 'getForUser');
+    Route::get('teams/{id}/designs', 'getForTeam');
+    Route::get('search/designs', 'search');
+});
 
-Route::get('users', [UserController::class, 'index']);
-Route::get('user/{username}', [UserController::class, 'findByUsername']);
-Route::get('users/{id}/designs', [DesignController::class, 'getForUser']);
+Route::controller(UserController::class)->group(function () {
+    Route::get('users', 'index');
+    Route::get('user/{username}', 'findByUsername');
+    Route::get('search/designers', 'search');
+});
 
 Route::get('teams/slug/{slug}', [TeamsController::class, 'findBySlug']);
-Route::get('teams/{id}/designs', [DesignController::class, 'getForTeam']);
-
-// Search Designs
-Route::get('search/designs', [DesignController::class, 'search']);
-Route::get('search/designers', [UserController::class, 'search']);
 
 /**
  * Routes for auth users only
  */
+
 Route::group([
     'middleware' => ['auth:api'],
 ], function () {
-    // Auth
     Route::post('logout', [LoginController::class, 'logout']);
 
     // User's settings
-    Route::put('settings/profile', [SettingsController::class, 'updateProfile']);
-    Route::put('settings/password', [SettingsController::class, 'updatePassword']);
+    Route::controller(SettingsController::class)->group(function () {
+        Route::put('settings/profile', 'updateProfile');
+        Route::put('settings/password', 'updatePassword');
+    });
 
-    // Design resource operations
+    // Design upload
     Route::post('designs', [UploadController::class, 'upload']);
-    Route::put('designs/{design}', [DesignController::class, 'update']);
-    Route::delete('designs/{design}', [DesignController::class, 'destroy']);
 
     // Comments
-    Route::post('designs/{id}/comments', [CommentController::class, 'store']);
-    Route::put('comments/{id}', [CommentController::class, 'update']);
-    Route::delete('comments/{id}', [CommentController::class, 'destroy']);
+    Route::controller(CommentController::class)->group(function () {
+        Route::post('designs/{id}/comments', 'store');
+        Route::put('comments/{id}', 'update');
+        Route::delete('comments/{id}', 'destroy');
+    });
 
-    // Likes and Unlikes
-    Route::post('designs/{id}/like', [DesignController::class, 'like']);
-    Route::get('designs/{id}/liked', [DesignController::class, 'checkIfUserHasLiked']);
+    // Designs
+    Route::controller(DesignController::class)->group(function () {
+        Route::post('designs/{id}/like', 'like');
+        Route::get('designs/{id}/liked', 'checkIfUserHasLiked');
+        Route::put('designs/{design}', 'update');
+        Route::delete('designs/{design}', 'destroy');
+    });
 
     // Teams
-    Route::post('teams', [TeamsController::class, 'store']);
-    Route::get('teams/{id}', [TeamsController::class, 'findById']);
-    Route::get('teams/', [TeamsController::class, 'index']);
-    Route::get('users/teams/', [TeamsController::class, 'fetchUserTeams']);
-    Route::put('teams/{id}', [TeamsController::class, 'update']);
-    Route::delete('teams/{id}', [TeamsController::class, 'destroy']);
-    Route::delete('teams/{teamId}/users/{userId}', [TeamsController::class, 'removeFromTeam']);
+    Route::controller(TeamsController::class)->group(function () {
+        Route::post('teams', 'store');
+        Route::get('teams/{id}', 'findById');
+        Route::get('teams/', 'index');
+        Route::get('users/teams/', 'fetchUserTeams');
+        Route::put('teams/{id}', 'update');
+        Route::delete('teams/{id}', 'destroy');
+        Route::delete('teams/{teamId}/users/{userId}', 'removeFromTeam');
+    });
 
     // Invitations
-    Route::post('invitations/{teamId}', [InvitationsController::class, 'invite']);
-    Route::post('invitations/{id}/resend', [InvitationsController::class, 'resend']);
-    Route::post('invitations/{id}/respond', [InvitationsController::class, 'respond']);
-    Route::delete('invitations/{id}', [InvitationsController::class, 'destroy']);
+    Route::controller(InvitationsController::class)->group(function () {
+        Route::post('invitations/{teamId}', 'invite');
+        Route::post('invitations/{id}/resend', 'resend');
+        Route::post('invitations/{id}/respond', 'respond');
+        Route::delete('invitations/{id}', 'destroy');
+    });
 
     // Chats
-    Route::get('chats', [ChatsController::class, 'getUserChats']);
-    Route::post('chats', [ChatsController::class, 'sendMessage']);
-    Route::get('chats/{id}/messages', [ChatsController::class, 'getChatMessages']);
-    Route::put('chats/{id}/markAsRead', [ChatsController::class, 'markAsRead']);
-    Route::delete('messages/{id}', [ChatsController::class, 'destroyMessage']);
+    Route::controller(ChatsController::class)->group(function () {
+        Route::get('chats', 'getUserChats');
+        Route::post('chats', 'sendMessage');
+        Route::get('chats/{id}/messages', 'getChatMessages');
+        Route::put('chats/{id}/markAsRead', 'markAsRead');
+        Route::delete('messages/{id}', 'destroyMessage');
+    });
 });
 
 /**
@@ -94,15 +109,18 @@ Route::group([
 Route::group([
     'middleware' => ['guest:api'],
 ], function () {
-    // Auth
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [LoginController::class, 'login']);
 
     // New user's verification
-    Route::post('verification/verify/{user}', [VerificationController::class, 'verify'])->name('verification.verify');
-    Route::post('verification/resend', [VerificationController::class, 'resend']);
+    Route::controller(VerificationController::class)->group(function () {
+        Route::post('verification/verify/{user}', 'verify')
+            ->name('verification.verify');
+        Route::post('verification/resend', 'resend');
+    });
 
     // Password
     Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
-    Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+    Route::post('password/reset', [ResetPasswordController::class, 'reset'])
+        ->name('password.reset');
 });
